a360c4d4e31a9eec8a6d9e111b61fb99d9222b67
Dillon <ddalton@ewu.edu>
2022-04-26 10:22:52 -0700

    created unit test file for main

--- ParkingMeter.java
+++ ParkingMeter.java
@@ -1,5 +1,4 @@
 public class ParkingMeter {
     public static void main(String[] args) {
-
     }
 }
---
+++ ParkingMeterTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+class ParkingMeterTest {
+
+    @Test
+    void mainTest() {
+    }
+
+
+}

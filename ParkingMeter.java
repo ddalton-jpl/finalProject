dcd0fa809884045a0e24ecc49d0b9d54a54a51b9
Dillon <ddalton@ewu.edu>
2022-04-27 10:03:53 -0700

    current changes to the parking meter class

--- ParkingMeter.java
+++ ParkingMeter.java
@@ -1,4 +1,17 @@
 public class ParkingMeter {
-    public static void main(String[] args) {
+    // 6-digit Identification # for a parking meter
+    private Integer parkingMeterNumber;
+
+    // The location of the parking meter
+    private String parkingMeterLocation;
+
+    // The duration you can park at a meter
+    private String parkingMeterTime;
+
+    // Constructor for the parking meter
+    public ParkingMeter(Integer parkingMeterNumber, String parkingMeterLocation, Integer parkingMeterTime) {
+        if (parkingMeterNumber > 999999 || parkingMeterNumber < 100000 || parkingMeterLocation.length() < 1) {
+            throw new IllegalArgumentException("Invalid Input");
+        }
     }
 }

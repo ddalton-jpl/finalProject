cdffbb3a4a36b92a79548e8643f43310e2111c10
Dillon <ddalton@ewu.edu>
2022-04-28 08:01:06 -0700

    added some more unit tests

--- ParkingMeter.java
+++ ParkingMeter.java
@@ -1,17 +1,32 @@
 public class ParkingMeter {
     // 6-digit Identification # for a parking meter
-    private Integer parkingMeterNumber;
+    private final Integer parkingMeterNumber;
 
     // The location of the parking meter
-    private String parkingMeterLocation;
+    private final String parkingMeterLocation;
 
     // The duration you can park at a meter
-    private String parkingMeterTime;
+    private final String parkingMeterTime;
 
     // Constructor for the parking meter
-    public ParkingMeter(Integer parkingMeterNumber, String parkingMeterLocation, Integer parkingMeterTime) {
-        if (parkingMeterNumber > 999999 || parkingMeterNumber < 100000 || parkingMeterLocation.length() < 1) {
+    public ParkingMeter(Integer parkingMeterNumber, String parkingMeterLocation, String parkingMeterTime) {
+        if (parkingMeterNumber > 999999 || parkingMeterNumber < 100000 || parkingMeterLocation.length() < 1 || !parkingMeterTime.matches("^(\\d|0\\d|1\\d|2[0-3]):[0-5]\\d$")) {
             throw new IllegalArgumentException("Invalid Input");
         }
+        this.parkingMeterNumber = parkingMeterNumber;
+        this.parkingMeterLocation = parkingMeterLocation;
+        this.parkingMeterTime = parkingMeterTime;
+    }
+
+    public String getParkingMeterTime() {
+        return parkingMeterTime;
+    }
+
+    public String getParkingMeterLocation() {
+        return parkingMeterLocation;
+    }
+
+    public Integer getParkingMeterNumber() {
+        return parkingMeterNumber;
     }
 }
--- ParkingMeterTest.java
+++ ParkingMeterTest.java
@@ -1,10 +1,44 @@
 import org.junit.jupiter.api.Test;
 
+import static org.junit.Assert.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class ParkingMeterTest {
 
+    Integer parkingMeterNumber = 123456;
+    String parkingMeterLocation = "8809 W Castle Street";
+    String parkingMeterTime = "10:40";
+    ParkingMeter parkingMeter = new ParkingMeter(parkingMeterNumber, parkingMeterLocation, parkingMeterTime);
+
     @Test
-    void mainTest() {
+    void ParkingMeterConstructor() {
+        assertEquals(parkingMeterNumber, parkingMeter.getParkingMeterNumber());
+        assertEquals(parkingMeterLocation, parkingMeter.getParkingMeterLocation());
+        assertEquals(parkingMeterTime, parkingMeter.getParkingMeterTime());
     }
 
+    @Test
+    void constructorException() {
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            Integer invalidParkingMeterNumber = 123;
+            String invalidParkingMeterLocation = "";
+            String invalidParkingMeterTime = "99:99";
+            ParkingMeter parkingMeter1 = new ParkingMeter(invalidParkingMeterNumber, invalidParkingMeterLocation, invalidParkingMeterTime);
+        });
+        String expectedMessage = "Invalid Input";
+        String actualMessage = exception.getMessage();
+        assertEquals(expectedMessage, actualMessage);
+    }
 
+    @Test
+    void getParkingMeterTime() {
+    }
+
+    @Test
+    void getParkingMeterLocation() {
+    }
+
+    @Test
+    void getParkingMeterNumber() {
+    }
 }

b96d7fd73bbfc36f8c25d279351f707433a41ab3
Dillon <ddalton@ewu.edu>
2022-05-02 07:43:14 -0700

    added parking meter tab class

--- src/ParkingMeter.java
+++ src/ParkingMeter.java
@@ -18,18 +18,14 @@ public class ParkingMeter {
     HashMap<Integer, ParkingMeter> hm = new HashMap<>();;
 
     public ParkingMeter(Integer parkingMeterNumber, String parkingMeterLocation, String parkingMeterTime) {
-        if (parkingMeterNumber > 999999 || parkingMeterNumber < 100000 || parkingMeterLocation.length() < 1 || !parkingMeterTime.matches("^(\\d|0\\d|1\\d|2[0-3]):[0-5]\\d$")) {
+        if (parkingMeterNumber > 999999 || parkingMeterNumber < 100000 || parkingMeterLocation.length() < 1 || !parkingMeterTime.matches("^(\\d|0\\d|1\\d|2[0-3]):[0-5]\\d$") || hm.containsKey(parkingMeterNumber)) {
             throw new IllegalArgumentException("Invalid Input");
         }
-        // TODO Store these parking meters in a hash map by hashing the parking meter #
-        // TODO only add parking meter if it is not already in the hashmap
-        // 6-digit Identification # for a parking meter
         this.parkingMeterLocation = parkingMeterLocation;
         this.timeAtMeter = parkingMeterTime;
         this.meterIsTaken = false;
         this.parkingMeterNumber = parkingMeterNumber;
-        hm.put(parkingMeterNumber, this);
-
+        hm.put(this.parkingMeterNumber, this);
     }
 
     public String getTimeAtMeter(Integer key) {
